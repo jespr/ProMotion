f37ebcbc160c74338b18d698ffcb1e7cf1340a64
Jesper Christiansen <jchristiansen@zendesk.com>
2013-07-13 18:22:37 -0700

    You can now specify title_view for custom views for your section titles
    def table_data
      [
        title: "My section title",
        title_view: MyCustomSection,
        title_height: 50,
        cells: []
      ]
    end
    
    You'll have to set a title accessor on your custom title view class, as the
    title you specified in table_data will be set on that attribute.
    
    class MyCustomSection
      attr_accessor :title
    end

--- lib/ProMotion/table/table_screen.rb
+++ lib/ProMotion/table/table_screen.rb
@@ -2,5 +2,29 @@ module ProMotion
   class TableScreen < TableViewController
     include ProMotion::ScreenModule
     include ProMotion::Table
+
+    def tableView(table_view, viewForHeaderInSection: index)
+      section = section_at_index(index)
+
+      if section[:title_view]
+        klass      = section[:title_view]
+        view       = klass.alloc.init
+        raise "#{klass} must have a 'title' accessor" unless view.respond_to?(:title)
+        view.title = section[:title]
+        view
+      else
+        nil
+      end
+    end
+
+    def tableView(table_view, heightForHeaderInSection: index)
+      section = section_at_index(index)
+
+      if section[:title_view] || (section[:title] && !section[:title].empty?)
+        section[:title_view_height] || tableView.sectionHeaderHeight
+      else
+        0.0
+      end
+    end
   end
 end
